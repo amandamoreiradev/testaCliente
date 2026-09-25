ea45c48a270599205aeaec0b676d8d67b0510c8f
amandacostamoreira <amandamoreiracosta012@gmail.com>
2026-09-25 14:52:50 -0300

    Add files via upload

---
+++ Endereco.java
@@ -0,0 +1,56 @@
+package com.mycompany.testacliente;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Amanda
+ */
+public class Endereco {
+    
+   private String rua;
+   private String cidade;
+   private String estado;
+
+    public Endereco(String rua, String cidade, String estado) {
+        this.rua = rua;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" + "rua=" + rua + ", cidade=" + cidade + ", estado=" + estado + '}';
+    }
+
+  
+    
+    
+}
